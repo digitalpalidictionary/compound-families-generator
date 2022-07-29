a126235dec3bc19382eea67b72b768d639bb286e
bdhrs <bodhirasa@gmail.com>
2022-07-29 08:09:00 +0530

    b2

--- compound family generator.py
+++ compound family generator.py
@@ -65,8 +65,7 @@ def generate_compound_families_html():
 
 	test1 = dpd_df["Family2"] != ""
 	test2 = dpd_df["Meaning IN CONTEXT"] != ""
-	# test3 = dpd_df["Pāli Root"] == ""
-	filter = test1 & test2 # & test3
+	filter = test1 & test2
 	df_reduced = dpd_df[filter]
 
 	compound_family_count = compound_family_df.shape[0]
@@ -99,7 +98,7 @@ def generate_compound_families_html():
 				cf_constr = df_filtered.iloc[row, 3]
 				cf_constr = re.sub (r"<br/>.+", "", cf_constr)
 			
-				html += f"""<b>{cf_pali}</b>&ensp;<b>{cf_pos}</b>&ensp;{cf_english}<br>"""
+				html += f"""<b>{cf_pali}</b>&ensp;<b2>{cf_pos}</b2>&ensp;{cf_english}<br>"""
 				anki_html += f"<tr valign='top'><div style='color: #FFB380'><td>{cf_pali}</td><td><div style='color: #FF6600'>{cf_pos}</div></td><td><div style='color: #FFB380'>{cf_english}</td><td><div style='color: #FF6600'>{cf_constr}</div></td></tr>"
 
 			html += f"</p>"
